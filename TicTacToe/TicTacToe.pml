73064fb6dfd9ddcaacd5403ebaf59c94e9563b4f
aprilmaraat <aprilmaraat@gmail.com>
2015-10-19 10:58:08 +0800

    File migration

---
+++ TicTacToe/TicTacToe.pml
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="TicTacToe" format_version="4">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="." xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs />
+    <Resources>
+        <File name="GameTable" src="html/GameTable.html" />
+        <File name="index" src="html/index.html" />
+        <File name="demo" src="html/css/demo.css" />
+        <File name="O" src="html/images/O.png" />
+        <File name="X" src="html/images/X.png" />
+        <File name="function" src="html/scripts/function.js" />
+        <File name="jquery-1.9.1" src="html/scripts/jquery-1.9.1.js" />
+        <File name="jquery.blockUI" src="html/scripts/jquery.blockUI.js" />
+    </Resources>
+    <Topics />
+    <IgnoredPaths>
+        <Path src=".DS_Store" />
+    </IgnoredPaths>
+</Package>
---
+++ TicTacToe/html/scripts/function.js
@@ -0,0 +1,85 @@
+var session = new QiSession();
+var userSymbol = "Symbol not set.";
+
+$(document).ready(function(){
+	Load(true);
+}); // end $(document).ready(function(){
+
+function assignSymbol(value){
+	userSymbol = value;
+	Load(false);
+}
+
+function pepperPlot(value){
+	var pepperSymbol = "";
+	if(userSymbol != "X")
+	{
+		pepperSymbol = "X";
+		$("#"+value).find("#back").css("background-color", "#FF6666");
+	}
+	else{
+		pepperSymbol = "O";
+		$("#"+value).find("#back").css("background-color", "#99CCFF");
+	}
+	$("#"+value).find("#back").html('<img style="padding: 0.5em;" src="images/'+ pepperSymbol +'.png">');
+	flipCard(document.getElementById(value).classList);
+}
+
+function showWinner(value){
+	alert(value);
+}
+
+// QiMessaging
+
+function startSubscribe() {
+	session.service("ALMemory").done(function (ALMemory) {
+		ALMemory.subscriber("TicTacToe/UserSymbol").done(function(subscriber) {
+			subscriber.signal.connect(assignSymbol);
+		});
+		ALMemory.subscriber("TicTacToe/PepperPlot").done(function(subscriber) {
+			subscriber.signal.connect(pepperPlot);
+		});
+		ALMemory.subscriber("TicTacToe/Winner").done(function(subscriber) {
+			subscriber.signal.connect(showWinner);
+		});
+	});
+}
+
+function endTurn(message) {
+	session.service("ALMemory").done(function (ALMemory) {
+		ALMemory.raiseEvent("TicTacToe/UserFinishTurn", message);
+	});
+}
+
+// end of QiMessaging
+
+function flipCard(c){
+	if(c.contains("flipped") != true){
+		c.add("flipped");
+	}
+}
+
+function plotUser(x){
+	if(userSymbol == "X")
+	{
+		$(x).find("#back").css("background-color", "#FF6666");
+	}
+	else{
+		$(x).find("#back").css("background-color", "#99CCFF");
+	}
+	$(x).find("#back").html('<img style="padding: 0.5em;" src="images/'+ userSymbol +'.png">');
+	endTurn(x.id);
+}
+
+function Load(bool){
+	if (bool) {
+		$.blockUI({
+			message: '<div height: 100px; ><div style="font-size: 25px; padding: 1.5em;">Please Wait...</div></div>',
+			css: { border: '2px solid #B8B8B8 ', cursor: 'wait', backgroundColor: '#FFFFFF' },
+			overlayCSS: { backgroundColor: '#000000', opacity: 0.5, cursor: 'wait' }
+		});
+	} 
+	else {
+		$.unblockUI();
+	}
+}
